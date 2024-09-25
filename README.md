e093a029d6dfc0d2e0ef7b3974be8fbc614b46e0
audrey <audrey@gobaco.com>
2024-09-24 21:25:46 -0700

    Introduction of std::env::args to support ordered flag functionality
    - now handles default hexdecimal (no option flags passed), normal hexdecimal with more spaces, and decimal
    - now uses std::env::args to maintain ordering when multiple -b -b passed for example
    - added (cloned) unit tests for hexdecimal and decimal

--- README.md
+++ README.md
@@ -35,5 +35,19 @@ Example Usage:
 000002a
 ```
 
+## Implementation Notes
+Some cool functionality that I wanted to feature in this implementation was the fact that you could print the hexadecimal, decimal, octal, or ascii representations of the file's bytes simultaneously, and in a specific noted order. For example:
+```bash
+❯ hexdump -b -x -c -n 32 examples/html.txt
+0000000 131 157 165 040 143 141 156 047 164 040 160 141 162 163 145 040
+0000000    6f59    2075    6163    276e    2074    6170    7372    2065
+0000000   Y   o   u       c   a   n   '   t       p   a   r   s   e
+0000010 133 130 135 110 124 115 114 040 167 151 164 150 040 162 145 147
+0000010    585b    485d    4d54    204c    6977    6874    7220    6765
+0000010   [   X   ]   H   T   M   L       w   i   t   h       r   e   g
+0000020
+```
+At this point in time, I realized that I didn't have a way to retain the order of the options given with `clap`, so I ended up additionally using the Rust standard library to fetch and handle certain option flags while retaining their order, like `-b`, manually. The `clap` `Args` struct remains as to at least verify the syntax of the arguments passed into the CLI tool--it parses flags like `-b` but doesn't actually do anything with them. My implementation handles the above case by creating an iterator struct for each of the option flags given, namely ones like `-b` `-x` `-c`, and storing them in an array. Each of the iterators in my array is called in order to print each representation. A "fun" thing to note is that the same option flag can be used multiple times, for example `-b -b`, in which the octal representation would be doubly printed. Though all two-byte printing styles have practically the same semantics, for example `TwoByteHexadecimal` and `TwoByteDecimal`, and could be kept within one unified `TwoByte` struct, I chose to divide them into their own structs as to avoid an O(1) check within `TwoByte`'s `next()` function when determining which numeric formatting to use. I based the output formatting from what I saw on my Macbook, so the output may differ from the actual `hexdump` command depending on the machine you're running it on.
+
 ## Credits
-The functionality of this tool is meant to mimic [`hexdump`](https://www.man7.org/linux/man-pages/man1/hexdump.1.html). Hence, many of the options and their explanations are taken directly from the manpage. In addition, `examples/bee_movie.txt` is a copy-paste the "Bee Movie (2007)" script. All credits go to the screenwriters.
+The functionality of this tool is meant to mimic [`hexdump`](https://www.man7.org/linux/man-pages/man1/hexdump.1.html). Hence, many of the options and their explanations are taken directly from the manpage. In addition, `examples/html.txt` is a copy-paste of (this notorious StackOverflow post)[https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags#:~:text=While%20arbitrary%20HTML%20with%20only%20a%20regex%20is] and `examples/bee_movie.txt` is a copy-paste the "Bee Movie (2007)" script. All credits go to the original authors of these works.
--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,22 @@ pub struct Args {
     /// Toggle length of file to print
     #[arg(short = 'n', long = "length")]
     pub length: Option<usize>, // especially large positive integers will fail
+
+    /// Print Two-Byte Hexadecimal
+    #[arg(short = 'x', long = "two-bytes-hex", action = clap::ArgAction::Count)]
+    pub hexadecimal: u8,
+
+    /// Print Two-Byte Decimal
+    #[arg(short = 'd', long = "two-bytes-decimal", action = clap::ArgAction::Count)]
+    pub decimal: u8,
+
+    // -b, --one-byte-octal
+    // -c, --one-byte-char
 }
 
 fn main() {
     let args = Args::parse();
+
     if let Ok(bytes) = fs::read(args.file.clone()) {
         // Determine Length
         let length: usize;
@@ -30,17 +42,34 @@ fn main() {
         if length == 0 {
             return
         }
+        
+        // Fetch Repr Flags
+        let mut iterators: Vec<Box<dyn Iterator<Item = String>>> = vec![];
+        for arg in std::env::args() {
+            match arg.as_str() {
+                "-x" | "--two-bytes-hex" => iterators.push(Box::new(TwoByteHexadecimal::new(length, &bytes))),
+                "-d" | "--two-bytes-decimal" => iterators.push(Box::new(TwoByteDecimal::new(length, &bytes))),
+                _ => {},
+            }
+        }
+        // No repr option flags passed, use default
+        if iterators.len() == 0 {
+            iterators.push(Box::new(DefaultHexadecimal::new(length, &bytes)));
+        }
 
         // Printing Work
-        // let mut idx: usize = 0;
-        let mut two_byte_hexadecimal = TwoByteHexadecimal::new(length, &bytes);
+        let mut stop: bool = false;
         loop {
-            if let Some(line) = two_byte_hexadecimal.next() {
-                println!("{line}");
-            } else {
-                println!("{:07x}", two_byte_hexadecimal.idx);
-                break;
+            for iterator in &mut iterators {
+                if let Some(line) = iterator.next() {
+                    println!("{line}");
+                } else { // all other iterators should be done as well
+                    println!("{:07x}", length);
+                    stop = true;
+                    break;
+                }
             }
+            if stop { break };
         }
     } else {
         eprintln!("Failed to read file {}.", args.file);
@@ -49,6 +78,45 @@ fn main() {
 
 // TODO: Create helper functions that return strings of formatted decimal, octal, and ascii respectively
 // Create unit tests for each of the helper functions...
+struct DefaultHexadecimal<'a> {
+    idx: usize,
+    length: usize,
+    bytes: &'a Vec<u8>,
+}
+
+impl<'a> DefaultHexadecimal<'a> {
+    fn new(length: usize, bytes: &'a Vec<u8>) -> Self {
+        DefaultHexadecimal {idx: 0, length, bytes}
+    }
+}
+
+impl<'a> Iterator for DefaultHexadecimal<'a> {
+    type Item = String;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == self.length { // nothing left to print
+            return None
+        }
+        let mut out: String = format!("{:07x} ", self.idx);
+        if 8 <= (self.length-self.idx)/2 { 
+            for _ in 0..8 {
+                out.push_str(&format!("{:02x}{:02x} ", self.bytes[self.idx+1], self.bytes[self.idx]));
+                self.idx += 2;
+            }
+        } else { // last line to be printed
+            while self.idx+1 < self.length {
+                out.push_str(&format!("{:02x}{:02x} ", self.bytes[self.idx+1], self.bytes[self.idx]));
+                self.idx += 2;
+            }
+            if self.idx != self.length { // deal with leftover odd
+                out.push_str(&format!("{:04x} ", self.bytes[self.idx]));
+                self.idx += 1;
+            }
+        }
+        Some(out)
+    }
+}
+
+
 struct TwoByteHexadecimal<'a> {
     idx: usize,
     length: usize,
@@ -67,19 +135,58 @@ impl<'a> Iterator for TwoByteHexadecimal<'a> {
         if self.idx == self.length { // nothing left to print
             return None
         }
-        let mut out: String = format!("{:07x} ", self.idx);
+        let mut out: String = format!("{:07x}    ", self.idx);
         if 8 <= (self.length-self.idx)/2 { 
             for _ in 0..8 {
-                out.push_str(&format!("{:x}{:x} ", self.bytes[self.idx+1], self.bytes[self.idx]));
+                out.push_str(&format!("{:02x}{:02x}    ", self.bytes[self.idx+1], self.bytes[self.idx]));
                 self.idx += 2;
             }
         } else { // last line to be printed
             while self.idx+1 < self.length {
-                out.push_str(&format!("{:x}{:x} ", self.bytes[self.idx+1], self.bytes[self.idx]));
+                out.push_str(&format!("{:02x}{:02x}    ", self.bytes[self.idx+1], self.bytes[self.idx]));
                 self.idx += 2;
             }
             if self.idx != self.length { // deal with leftover odd
-                out.push_str(&format!("00{:x}", self.bytes[self.idx]));
+                out.push_str(&format!("{:04x}    ", self.bytes[self.idx]));
+                self.idx += 1;
+            }
+        }
+        Some(out)
+    }
+}
+
+
+struct TwoByteDecimal<'a> {
+    idx: usize,
+    length: usize,
+    bytes: &'a Vec<u8>,
+}
+
+impl<'a> TwoByteDecimal<'a> {
+    fn new(length: usize, bytes: &'a Vec<u8>) -> Self {
+        TwoByteDecimal {idx: 0, length, bytes}
+    }
+}
+
+impl<'a> Iterator for TwoByteDecimal<'a> {
+    type Item = String;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == self.length { // nothing left to print
+            return None
+        }
+        let mut out: String = format!("{:07x}   ", self.idx);
+        if 8 <= (self.length-self.idx)/2 { 
+            for _ in 0..8 {
+                out.push_str(&format!("{:05}   ", ((self.bytes[self.idx+1] as u16) << 8)+(self.bytes[self.idx] as u16)));
+                self.idx += 2;
+            }
+        } else { // last line to be printed
+            while self.idx+1 < self.length {
+                out.push_str(&format!("{:05}   ", ((self.bytes[self.idx+1] as u16) << 8)+(self.bytes[self.idx] as u16)));
+                self.idx += 2;
+            }
+            if self.idx != self.length { // deal with leftover odd
+                out.push_str(&format!("{:05}   ", self.bytes[self.idx]));
                 self.idx += 1;
             }
         }
@@ -91,25 +198,67 @@ impl<'a> Iterator for TwoByteHexadecimal<'a> {
 
 #[cfg(test)]
 mod tests {
-    use crate::TwoByteHexadecimal;
+    use super::*;
+    #[test]
+    fn test_default_hexadecimal() {
+        let bytes: Vec<u8> = "orange juice and banana peel! apple juice and lemon rind!".as_bytes().to_vec();
+        // Multiple Lines, Odd Last Line
+        let mut mult_iterator = DefaultHexadecimal::new(bytes.len(), &bytes);
+        assert_eq!(mult_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000010 6220 6e61 6e61 2061 6570 6c65 2021 7061 ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000020 6c70 2065 756a 6369 2065 6e61 2064 656c ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000030 6f6d 206e 6972 646e 0021 ".to_string());
+        assert_eq!(mult_iterator.next(), None);
+        // Full Last Line
+        let mut full_iterator = DefaultHexadecimal::new(16, &bytes);
+        assert_eq!(full_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
+        assert_eq!(full_iterator.next(), None);
+        // Even Last Line
+        let mut even_iterator = DefaultHexadecimal::new(30, &bytes);
+        assert_eq!(even_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
+        assert_eq!(even_iterator.next().unwrap(), "0000010 6220 6e61 6e61 2061 6570 6c65 2021 ".to_string());
+        assert_eq!(even_iterator.next(), None);
+    }
+
     #[test]
     fn test_two_byte_hexadecimal() {
         let bytes: Vec<u8> = "orange juice and banana peel! apple juice and lemon rind!".as_bytes().to_vec();
         // Multiple Lines, Odd Last Line
         let mut mult_iterator = TwoByteHexadecimal::new(bytes.len(), &bytes);
-        assert_eq!(mult_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
-        assert_eq!(mult_iterator.next().unwrap(), "0000010 6220 6e61 6e61 2061 6570 6c65 2021 7061 ".to_string());
-        assert_eq!(mult_iterator.next().unwrap(), "0000020 6c70 2065 756a 6369 2065 6e61 2064 656c ".to_string());
-        assert_eq!(mult_iterator.next().unwrap(), "0000030 6f6d 206e 6972 646e 0021".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000000    726f    6e61    6567    6a20    6975    6563    6120    646e    ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000010    6220    6e61    6e61    2061    6570    6c65    2021    7061    ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000020    6c70    2065    756a    6369    2065    6e61    2064    656c    ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000030    6f6d    206e    6972    646e    0021    ".to_string());
         assert_eq!(mult_iterator.next(), None);
         // Full Last Line
         let mut full_iterator = TwoByteHexadecimal::new(16, &bytes);
-        assert_eq!(full_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
+        assert_eq!(full_iterator.next().unwrap(), "0000000    726f    6e61    6567    6a20    6975    6563    6120    646e    ".to_string());
         assert_eq!(full_iterator.next(), None);
         // Even Last Line
         let mut even_iterator = TwoByteHexadecimal::new(30, &bytes);
-        assert_eq!(even_iterator.next().unwrap(), "0000000 726f 6e61 6567 6a20 6975 6563 6120 646e ".to_string());
-        assert_eq!(even_iterator.next().unwrap(), "0000010 6220 6e61 6e61 2061 6570 6c65 2021 ".to_string());
+        assert_eq!(even_iterator.next().unwrap(), "0000000    726f    6e61    6567    6a20    6975    6563    6120    646e    ".to_string());
+        assert_eq!(even_iterator.next().unwrap(), "0000010    6220    6e61    6e61    2061    6570    6c65    2021    ".to_string());
+        assert_eq!(even_iterator.next(), None);
+    }
+
+    #[test]
+    fn test_two_byte_decimal() {
+        let bytes: Vec<u8> = "orange juice and banana peel! apple juice and lemon rind!".as_bytes().to_vec();
+        // Multiple Lines, Odd Last Line
+        let mut mult_iterator = TwoByteDecimal::new(bytes.len(), &bytes);
+        assert_eq!(mult_iterator.next().unwrap(), "0000000   29295   28257   25959   27168   26997   25955   24864   25710   ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000010   25120   28257   28257   08289   25968   27749   08225   28769   ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000020   27760   08293   30058   25449   08293   28257   08292   25964   ".to_string());
+        assert_eq!(mult_iterator.next().unwrap(), "0000030   28525   08302   26994   25710   00033   ".to_string());
+        assert_eq!(mult_iterator.next(), None);
+        // Full Last Line
+        let mut full_iterator = TwoByteDecimal::new(16, &bytes);
+        assert_eq!(full_iterator.next().unwrap(), "0000000   29295   28257   25959   27168   26997   25955   24864   25710   ".to_string());
+        assert_eq!(full_iterator.next(), None);
+        // Even Last Line
+        let mut even_iterator = TwoByteDecimal::new(30, &bytes);
+        assert_eq!(even_iterator.next().unwrap(), "0000000   29295   28257   25959   27168   26997   25955   24864   25710   ".to_string());
+        assert_eq!(even_iterator.next().unwrap(), "0000010   25120   28257   28257   08289   25968   27749   08225   ".to_string());
         assert_eq!(even_iterator.next(), None);
     }
 }
